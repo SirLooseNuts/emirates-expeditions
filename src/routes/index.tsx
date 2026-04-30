abdabe1b7db2a268ccfcecb80dd89f6200411027
Banna <bannafn517@gmail.com>
2026-04-30 22:05:54 +0530

    Revert partner tiles to 4 columns (4x3 layout)

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -185,7 +185,7 @@ function Home() {
           <div 
             ref={partnersRef} 
             onMouseMove={handleMouseMove} 
-            className="group/bento mt-12 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3"
+            className="group/bento mt-12 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4"
           >
             {partners.map((p) => (
               <div 
