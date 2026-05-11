6fb7308fc26f42b636c001391c0eb7e66beeb770
Banna <bannafn517@gmail.com>
2026-05-11 19:10:02 +0530

    Update Udupi destination photo and sync all udupi1 gallery references

--- src/data/tours.ts
+++ src/data/tours.ts
@@ -28,7 +28,7 @@ import hampi1 from "@/assets/tour-marayoor.png";
 import goa1 from "@/assets/tour-3day-alleppey.png";
 import dandeli1 from "@/assets/tour-wayanad-wildlife.png";
 import malpe1 from "@/assets/tour-5day-varkala.png";
-import udupi1 from "@/assets/tour-coorg.png";
+import udupi1 from "@/assets/tours/destinations/udupi.jpg";
 import mistsStone from "@/assets/tour-mists-stone.jpg";
 import vagamonKodaiMist from "@/assets/tour-vagamon-kodai-mist-2.jpg";
 import kodaiMist from "@/assets/tour-kodai-mist.jpg";
